c03fd01cbee7374cf818a733d135de87cb4c9659
arabbuc <abarmando@gmail.com>
2020-06-25 13:14:41 +0200

    subido crear servicios y ts de lista-servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,22 @@ import { ProveedorListComponent } from './components/proveedores/proveedor-list/
 import { ProveedorCreateComponent } from './components/proveedores/proveedor-create/proveedor-create.component';
 import { ProveedorComponent } from './components/proveedores/proveedor/proveedor.component';
 import { HomeComponent } from './components/home/home.component';
+import { ServicioListComponent } from './components/servicios/servicio-list/servicio-list.component';
+import { ServicioCreateComponent } from './components/servicios/servicio-create/servicio-create.component';
+import { ServicioComponent } from './components/servicios/servicio/servicio.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'proveedor/lista', component: ProveedorListComponent },
   { path: 'proveedor/crear', component: ProveedorCreateComponent },
   { path: 'proveedor/editar/:id', component: ProveedorCreateComponent },
-  { path: 'proveedor/:id', component: ProveedorComponent}
+  { path: 'proveedor/:id', component: ProveedorComponent},
+
+  { path: 'servicio/lista', component: ServicioListComponent },
+  { path: 'servicio/crear', component: ServicioCreateComponent },
+  { path: 'servicio/editar/:id', component: ServicioCreateComponent },
+  { path: 'servicio/:id', component: ServicioComponent }
+
 ];
 
 @NgModule({
