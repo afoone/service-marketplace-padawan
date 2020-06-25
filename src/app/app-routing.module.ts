29bf97d3a7d22f611ee34d1f7b2127f53efd39b3
Your Name <you@example.com>
2020-06-25 14:05:24 +0200

    merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,18 +22,16 @@ const routes: Routes = [
   { path: 'proveedor/editar/:id', component: ProveedorCreateComponent },
   { path: 'proveedor/:id', component: ProveedorComponent},
 
-<<<<<<< HEAD
   { path: 'usario/lista', component: UsuarioListComponent},
   { path: 'usuario/crear', component: UsuarioCreateComponent},
   { path: 'usuario/editar/:id', component: UsuarioCreateComponent},
-  { path: 'usuario/:id', component: UsuarioComponent}
-=======
+  { path: 'usuario/:id', component: UsuarioComponent},
+  
   { path: 'servicio/lista', component: ServicioListComponent },
   { path: 'servicio/crear', component: ServicioCreateComponent },
   { path: 'servicio/editar/:id', component: ServicioCreateComponent },
   { path: 'servicio/:id', component: ServicioComponent }
 
->>>>>>> e25866992786fa1d588591ba116d106c0c267ba1
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,12 @@ import { ProveedorCreateComponent } from './components/proveedores/proveedor-cre
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
 import { UsuarioComponent } from './components/usuarios/usuario/usuario.component';
 import { UsuarioCreateComponent } from './components/usuarios/usuario-create/usuario-create.component';
 import { UsuarioListComponent } from './components/usuarios/usuario-list/usuario-list.component';
-=======
 import { ServicioComponent } from './components/servicios/servicio/servicio.component';
 import { ServicioCreateComponent } from './components/servicios/servicio-create/servicio-create.component';
 import { ServicioListComponent } from './components/servicios/servicio-list/servicio-list.component';
->>>>>>> e25866992786fa1d588591ba116d106c0c267ba1
 
 @NgModule({
   declarations: [
@@ -29,15 +26,12 @@ import { ServicioListComponent } from './components/servicios/servicio-list/serv
     ProveedorCreateComponent,
     HeaderComponent,
     HomeComponent,
-<<<<<<< HEAD
     UsuarioComponent,
     UsuarioCreateComponent,
-    UsuarioListComponent
-=======
+    UsuarioListComponent,
     ServicioComponent,
     ServicioCreateComponent,
     ServicioListComponent
->>>>>>> e25866992786fa1d588591ba116d106c0c267ba1
   ],
   imports: [
     BrowserModule,
