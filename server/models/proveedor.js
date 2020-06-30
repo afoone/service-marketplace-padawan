c900ef4fc4a386551554eb48f57c9a5a9aca0352
Alfonso Tienda <atienda@iprocuratio.com>
2020-06-30 13:18:51 +0200

    relaciones y populate

--- server/models/proveedor.js
+++ server/models/proveedor.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
-const proveedorSchema = mongoose.Schema( {
-        nombre: String,
-        servicio: String,
-        descripcion: String,
-        cif: String,
-        correo: String,
-        telefono: String,
-        imagen: String,
-    }
+
+const proveedorSchema = mongoose.Schema({
+    nombre: String,
+    descripcion: String,
+    cif: String,
+    correo: String,
+    telefono: String,
+    imagen: String,
+    servicio: [{ type: mongoose.Schema.Types.ObjectId, ref: "Servicio" }]
+}
 )
 
 module.exports = mongoose.model('Proveedor', proveedorSchema);
--- server/models/servicio.js
+++ server/models/servicio.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const servicioSchema = mongoose.Schema( {
-        nombre: String,
-        precio: String,
-        descripcion: String,
-        imagen: String
-
-    }
+const servicioSchema = mongoose.Schema({
+    nombre: String,
+    precio: String,
+    descripcion: String,
+    imagen: String,
+    proveedor: [{ type: mongoose.Schema.Types.ObjectId, ref: "Proveedor" }]
+}
 )
 
 module.exports = mongoose.model('Servicio', servicioSchema);
